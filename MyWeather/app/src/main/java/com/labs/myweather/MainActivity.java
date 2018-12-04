f85dff2a2b99f93ff3c7f9da6742277f14a1fd73
Tim Brychikov <42029597+vampir9939@users.noreply.github.com>
2018-12-04 11:47:05 +0300

    OutRequest

--- MyWeather/app/src/main/java/com/labs/myweather/MainActivity.java
+++ MyWeather/app/src/main/java/com/labs/myweather/MainActivity.java
@@ -3,12 +3,15 @@ package com.labs.myweather;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
 
 import static android.widget.Toast.LENGTH_SHORT;
@@ -38,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
     Button bSity2;
     Button bSity3;
 
+    TextView mDayPos;
+    TextView mTemp;
+    TextView mHumidity;
+    TextView mWindSpeed;
+    TextView mPressure;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +63,19 @@ public class MainActivity extends AppCompatActivity {
         bSity2 = (Button)findViewById(R.id.bSity2);
         bSity3 = (Button)findViewById(R.id.bSity3);
 
+        mDayPos = (TextView)findViewById(R.id.mDayPos);
+        mTemp = (TextView)findViewById(R.id.mTemp);
+        mHumidity = (TextView)findViewById(R.id.mHumidity);
+        mWindSpeed = (TextView)findViewById(R.id.mWindSpeed);
+        mPressure = (TextView)findViewById(R.id.mPressure);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
         CurrentTime currentTime = new CurrentTime();
         new Thread(currentTime).start();
     }
 
     public void onCliclRefresh(View view) {
-        FindWeather();
+        UpdatingTargetWeather("Minsk");
     }
 
     @SuppressLint("ResourceAsColor")
@@ -67,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
                     bSity1.setBackgroundColor(getResources().getColor(colorSity));
                     bSity2.setBackgroundColor(getResources().getColor(colorWite));
                     bSity3.setBackgroundColor(getResources().getColor(colorWite));
+                    if(bSity1.getText().toString() != getResources().getString(R.string._1_sity)) {
+                        UpdatingTargetWeather(bSity1.getText().toString());
+                    }
                 }
                 else {
                     Intent intent = new Intent(this, SearchSity.class);
@@ -80,6 +103,9 @@ public class MainActivity extends AppCompatActivity {
                     bSity1.setBackgroundColor(getResources().getColor(colorWite));
                     bSity2.setBackgroundColor(getResources().getColor(colorSity));
                     bSity3.setBackgroundColor(getResources().getColor(colorWite));
+                    if(bSity2.getText() != getResources().getString(R.string._2_sity)) {
+                        UpdatingTargetWeather(bSity2.getText().toString());
+                    }
                 }
                 else {
                     Intent intent = new Intent(this, SearchSity.class);
@@ -93,6 +119,9 @@ public class MainActivity extends AppCompatActivity {
                     bSity1.setBackgroundColor(getResources().getColor(colorWite));
                     bSity2.setBackgroundColor(getResources().getColor(colorWite));
                     bSity3.setBackgroundColor(getResources().getColor(colorSity));
+                    if(bSity3.getText() != getResources().getString(R.string._3_sity)) {
+                        UpdatingTargetWeather(bSity3.getText().toString());
+                    }
                 }
                 else {
                     Intent intent = new Intent(this, SearchSity.class);
@@ -110,9 +139,9 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode,resultCode,data);
 
         if(resultCode == RESULT_OK){
+            String str = data.getStringExtra("ans");
             switch (requestCode){
                 case 1:
-                    String str = data.getStringExtra("ans");
                     switch (pos_sity) {
                         case 1:
                             bSity1.setText(str);
@@ -126,25 +155,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
             }
+            UpdatingTargetWeather(str);
         }
     }
 
-    public void FindWeather() {
+    public class _package {
+        public String temp;
+        public String huidity;
+        public String wind_speed;
+        public String pressure;
+        public String id_icon;
+    }
+
+    public void UpdatingTargetWeather(String Sity) {
         final Context context = this;
         Log.d("request","startRequest");
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=88d38b3803dc8b14d526103d3b9f6474";
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + Sity + "&appid=88d38b3803dc8b14d526103d3b9f6474";
 
         JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     JSONObject main_object = response.getJSONObject("main");
-                    String temp = String.valueOf(main_object.getDouble("temp") - 273);
+                    String temp = String.valueOf(main_object.getInt("temp") - 273);
                     String humidity = String.valueOf(main_object.getInt("humidity"));
                     String pressure = String.valueOf(main_object.getInt("pressure"));
 
                     JSONObject wind_object = response.getJSONObject("wind");
                     String wind_speed = String.valueOf(wind_object.getInt("speed"));
+
+                    JSONArray arr = response.getJSONArray("weather");
+                    JSONObject icon_object = arr.getJSONObject(0);
+                    String icon_id = String.valueOf(icon_object.getString("icon"));
 
                     Log.d("request",response.toString());
 
@@ -152,6 +194,20 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("request",humidity + "влажность");
                     Log.d("request",wind_speed + "скорость ветра");
                     Log.d("request", pressure + "давление");
+
+                    mDayPos.setText("День");
+                    mTemp.setText(temp + "°С");
+                    mHumidity.setText(humidity + "% влажности");
+                    mWindSpeed.setText(wind_speed + " м/с");
+                    mPressure.setText(pressure + " мм рт.ст.");
+
+                    LoadImage loadImage = null;
+                    try {
+                        loadImage = new LoadImage(new URL("http://api.openweathermap.org/img/w/" + icon_id +".png"), imageView);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    new Thread(loadImage).start();
 
                 } catch (JSONException e) {
                     Log.d("request","error" + e.toString());
@@ -208,6 +264,37 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+    class LoadImage implements Runnable {
+        boolean stopMode;
+        URL url;
+        ImageView imageView;
+
+        LoadImage(URL url, ImageView imageView) {
+            this.stopMode = false;
+            this.url = url;
+            this.imageView = imageView;
+        }
+
+        public void StopThread(){
+            this.stopMode = true;
+        }
+
+        @Override
+        public void run() {
+            Bitmap icon;
+            try {
+                Log.d("loadBitMap", "Start load");
+                InputStream in = new java.net.URL(url.toString()).openStream();
+                icon = BitmapFactory.decodeStream(in);
+                Log.d("loadBitMap", icon.toString());
+                this.imageView.setImageBitmap(icon);
+            } catch (Exception e) {
+                Log.d("loadBitMap", e.toString());
+                e.printStackTrace();
+            }
+            Log.d("loadBitMap", "finish load");
         }
     }
 }
